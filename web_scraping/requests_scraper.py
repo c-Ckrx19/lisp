1b3eb07c0f13c75f971053cf714478965c99ee44
c_ckrx <zhirui97@qq.com>
2022-05-07 16:31:40 +0800

    updated documenting code

--- web_scraping/requests_scraper.py
+++ web_scraping/requests_scraper.py
@@ -3,19 +3,61 @@ import requests
 
 
 class RequestsScraper(ABC):
+    """
+    A scraper using requests library.
+
+    Attributes
+    ----------
+    user_agent : str
+        user agent parameter in header
+    cookie : str
+        cookie parameter in header
+
+    Methods
+    -------
+    parse()
+        Abstract method for parse event
+    store()
+        Abstract method for data storage
+    run()
+        Run a Selenium scraper
+    """
     def __init__(self, user_agent, cookie):
+        """
+        Parameters
+        ----------
+        user_agent : str
+            user agent parameter in header
+        cookie : str
+            cookie parameter in header
+        """
         self.headers = {
             'User-Agent': user_agent,
             'Cookie': cookie}
     
     @abstractmethod
     def parse(self):
-        pass
+        """Customize your parse method.
+
+        Raises
+        ------
+        NotImplementedError
+            If the function is not overwritten.
+        """
+        raise NotImplementedError
 
     @abstractmethod
     def store(self):
-        pass
+        """Customize your store method.
+        
+        Raises
+        ------
+        NotImplementedError
+            If the function is not overwritten.
+        """
+        raise NotImplementedError
     
     def run(self):
+        """Run a requests scraper."""
         self.parse()
         self.store()
--- web_scraping/selenium_scraper.py
+++ web_scraping/selenium_scraper.py
@@ -6,13 +6,39 @@ import time
 
 
 class SeleniumScraper(ABC):
+    """
+    A scraper using Selenium library
+
+    Methods
+    -------
+    visit_web(driver, url)
+        Return an etree element of a web page
+    etree_after_click(driver)
+        Return an etree element of a web page visited via clicks
+    parse()
+        Abstract method for parse event
+    store()
+        Abstract method for data storage
+    run()
+        Run a Selenium scraper
+    """
     def __init__(self):
         self.driver = webdriver.Firefox()
 
     def __del__(self):
+        """Delete a driver after having finished using."""
         self.driver.quit()
 
     def visit_web(self, driver, url):
+        """Return an etree element of a web page.
+
+        Parameters
+        ----------
+        driver : selenium.webdriver
+            The web driver performing visit actions
+        url : str
+            URL of the web page visited
+        """
         driver.get(url)
         time.sleep(5)
         page = Soup(driver.page_source, features="html.parser")
@@ -20,6 +46,13 @@ class SeleniumScraper(ABC):
         return html_etree
 
     def etree_after_click(self, driver):
+        """Return an etree element of a web page visited via clicks.
+
+        Parameters
+        ----------
+        driver : selenium.webdriver
+              The web driver performing visit actions
+        """
         time.sleep(5)
         page = Soup(driver.page_source, features="html.parser")
         html_etree = etree.HTML(str(page))
@@ -27,12 +60,27 @@ class SeleniumScraper(ABC):
 
     @abstractmethod
     def parse(self):
+        """Customize your parse method.
+        
+        Raises
+        ------
+        NotImplementedError
+            If the function is not overwritten.
+        """
         raise NotImplementedError
 
     @abstractmethod
     def store(self):
+        """Customize your store method.
+        
+        Raises
+        ------
+        NotImplementedError
+            If the function is not overwritten.
+        """
         raise NotImplementedError
     
     def run(self):
+        "Run a Selenium scraper."
         self.parse()
         self.store()
